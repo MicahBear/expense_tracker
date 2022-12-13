77d8de3df2bd8d9e09366f36af95b9f226e07f83
MicahBear <micah.b.elm@gmail.com>
2022-12-13 09:06:25 -0800

    transaction list code block update

--- src/components/transactionList.js
+++ src/components/transactionList.js
@@ -4,7 +4,13 @@ import React from 'react'
 export const TransactionList = () => {
     return (
         <div>
-
+            <h3>History</h3>
+            <ul id="list" class="list">
+                {/* later on the li will be replaced with transaction component */}
+                <li class="minus">
+                    Cash <span>-$400</span><button class="delete-btn">x</button>
+                </li>
+            </ul>
         </div>
     )
 }
