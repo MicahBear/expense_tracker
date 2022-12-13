6ade9cddff95fd86a0ed229ca7ab1aaacdb4caf1
MicahBear <micah.b.elm@gmail.com>
2022-12-13 09:16:24 -0800

    update: remove id from components

--- src/components/balance.js
+++ src/components/balance.js
@@ -6,7 +6,7 @@ export const Balance = () => {
             <h4>
                 Your Balance
             </h4>
-            <h1 id="balance">$0.00</h1>
+            <h1 >$0.00</h1>
         </div>
     )
 }
