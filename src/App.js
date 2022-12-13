479d94c277793b53f62d9d7df1fdd5180371f05b
MicahBear <micah.b.elm@gmail.com>
2022-12-13 08:40:20 -0800

    create income expenses

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Balance } from './components/balance';
 import { Header } from './components/header';
+import { IncomeExpenses } from './components/incomeexpenses';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <div className='container'>
         < Balance />
+        < IncomeExpenses />
       </div>
     </div>
   );
